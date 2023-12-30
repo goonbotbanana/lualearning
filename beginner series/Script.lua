ed84d08ae24bdd01855be043a9aec8a0d0ee5af9
Pharit Smitasin <pharitsmitasin@gmail.com>
2023-12-30 14:21:11 -0500

    add realism to rain using random, adjusting properties

--- beginner series/Script.lua
+++ beginner series/Script.lua
@@ -115,13 +115,17 @@ newPart.Size = Vector3.new(3,10,1)
 newPart.Position = Vector3.new(1, 20, 0)
 
 while true do 
-	wait(0.2)
+	wait(0.1)
 	local Rain = Instance.new("Part", game.Workspace)
-	Rain.Position = Vector3.new(0, 15, 0)
-	Rain.Size = Vector3.new(0.25, 1.5, 0.25)
+	local Rain_x = math.random(0, 50)
+	local Rain_z = math.random(0, 50)
+	Rain.Position = Vector3.new(Rain_x, 40, Rain_z)
+	Rain.Size = Vector3.new(0.15, 1.25, 0.15)
 	Rain.Anchored = false
-	Rain.Transparency = 0.5
+	Rain.Transparency = 0.3
 	Rain.Color = Color3.fromRGB(73, 146, 255)
+	Rain.CanCollide = false
+	Rain.CastShadow = false
 end
 
 
